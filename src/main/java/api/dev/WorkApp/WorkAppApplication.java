f6d08910e400100468d3df98fdcf6f41805c6443
Kenneth C Ashwood jr <ashwood.kenneth@gmail.com>
2023-05-16 13:28:23 -0400

    Setup Dev Project Skeleton

---
+++ src/main/java/api/dev/WorkApp/WorkAppApplication.java
@@ -0,0 +1,13 @@
+package api.dev.WorkApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/dev/WorkApp/WorkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package api.dev.WorkApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
