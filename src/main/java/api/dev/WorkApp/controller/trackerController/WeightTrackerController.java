1d45ab70f31818ffcc25e3044d7b4d06532bc6af
Kenneth C Ashwood jr <ashwood.kenneth@gmail.com>
2023-06-27 21:45:27 -0400

    WIP fixed url request

--- src/main/java/api/dev/WorkApp/controller/trackerController/WeightTrackerController.java
+++ src/main/java/api/dev/WorkApp/controller/trackerController/WeightTrackerController.java
@@ -3,22 +3,20 @@ package api.dev.WorkApp.controller.trackerController;
 
 import api.dev.WorkApp.service.trackerService.WeightTrackerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/tracker")
 public class WeightTrackerController {
 
     @Autowired
     WeightTrackerService weightTrackerService;
 
-    @PutMapping("/{uid}/tracker")
+    @PutMapping("/{uid}")
     public String createWeightTracker(@PathVariable String uid, @RequestBody String targetWeight){
         String res = weightTrackerService.createWeightTracker(uid, targetWeight);
         return res;
-
     }
+
 
 }
